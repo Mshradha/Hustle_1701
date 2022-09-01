ff8c54b6333e480c14f090145c88c5a131ee273d
parthdeshmukh2 <parthdeshmukh99@gmail.com>
2022-09-01 19:57:23 +0530

    front end

---
+++ Frontend/masai/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './Components/Navbar.';
+import Info from './Components/Info';
+
+function App() {
+  return (
+    <div className="App">
+     <Navbar/>
+     <Info/>
+    </div>
+  );
+}
+
+export default App;
---
+++ Frontend/masai/src/Components/Info.jsx
@@ -0,0 +1,47 @@
+import { Box, Button, Text,  Drawer,
+    DrawerBody,
+    DrawerFooter,
+    DrawerHeader,
+    DrawerOverlay,
+    DrawerContent,
+    DrawerCloseButton,Input, useDisclosure } from '@chakra-ui/react'
+import React from 'react'
+
+const Info = () => {
+
+    const { isOpen, onOpen, onClose } = useDisclosure()
+  const btnRef = React.useRef()
+  return (
+    <Box w={{base:"90%", sm:"90%", md:"70%", lg:"50%"}}   m='auto' >
+
+<Drawer
+        isOpen={isOpen}
+        placement='right'
+        onClose={onClose}
+        finalFocusRef={btnRef}
+      >
+        <DrawerOverlay />
+        <DrawerContent>
+          <DrawerCloseButton />
+          <DrawerHeader>Create your account</DrawerHeader>
+
+          <DrawerBody>
+            <Input placeholder='Type here...' />
+          </DrawerBody>
+
+          <DrawerFooter>
+            <Button variant='outline' mr={3} onClick={onClose}>
+              Cancel
+            </Button>
+            <Button colorScheme='blue'>Save</Button>
+          </DrawerFooter>
+        </DrawerContent>
+      </Drawer>
+        <Text fontWeight='600' fontSize={{base:'25px', md:'40px' , lg:'50px'}} color='gray.600'  >Know More About Masai Here!</Text>
+         <Text fontWeight='800' fontSize={{base:'25px', md:'40px' , lg:'45px'}}  mt='4'    >Driven by Outcomes,<br/> Fuelled by Ambitions.</Text> 
+            <Button onClick={onOpen} bg='rgb(237,3,49)' color='white' mt='8' p='6'>To Know More, Click Here</Button>
+    </Box>
+  )
+}
+
+export default Info
